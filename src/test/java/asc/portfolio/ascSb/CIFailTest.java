ea6444463d9f1b011e075b9a6e3a4387c2e2b037
ch-yang1273 <ych1273@gmail.com>
2023-04-14 15:22:03 +0900

    test: CI Fail Test

---
+++ src/test/java/asc/portfolio/ascSb/CIFailTest.java
@@ -0,0 +1,13 @@
+package asc.portfolio.ascSb;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CIFailTest {
+
+    @Test
+    @DisplayName("This test should fail")
+    public void test() {
+        assert false;
+    }
+}
