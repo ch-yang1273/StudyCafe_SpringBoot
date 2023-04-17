0a2019e51c2f2dcae6e07ca48a5119e88fe07208
ch-yang1273 <ych1273@gmail.com>
2023-04-18 00:00:48 +0900

    test: SeatUsageEndingSoonEvent 발행 및 Litener 테스트

---
+++ src/test/java/asc/portfolio/ascSb/_integrational/reservation/SeatUsageEventListenerTest.java
@@ -0,0 +1,44 @@
+package asc.portfolio.ascSb._integrational.reservation;
+
+import asc.portfolio.ascSb.common.domain.CommonEventsPublisher;
+import asc.portfolio.ascSb.reservation.service.SeatUsageEventListener;
+import asc.portfolio.ascSb.seat.dto.SeatUsageEndingSoonEvent;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.event.ApplicationEvents;
+import org.springframework.test.context.event.RecordApplicationEvents;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RecordApplicationEvents
+@SpringBootTest
+class SeatUsageEventListenerTest {
+
+    @Autowired
+    private CommonEventsPublisher eventsPublisher;
+
+    @SpyBean
+    private SeatUsageEventListener listener;
+
+    @Autowired
+    private ApplicationEvents events;
+
+    @Test
+    void raise() {
+        eventsPublisher.raise(new SeatUsageEndingSoonEvent(1L, LocalDateTime.now()));
+
+        // 1. Mockito로 EventListener 가 호출되었는지 확인
+        verify(listener, times(1)).handleEndingSoonEvent(any());
+
+        // 2. @RecordApplicationEvents 발행된 SeatUsageEndingSoonEvent 횟수를 확인
+        long count = events.stream(SeatUsageEndingSoonEvent.class).count();
+        Assertions.assertThat(count).isEqualTo(1L);
+    }
+}
